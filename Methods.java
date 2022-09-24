baed074d09a2ceed0a8aa0507b2dc374b517fd9d
ferhat-atas <93721598+ferhat-atas@users.noreply.github.com>
2022-09-24 17:11:11 +0300

    Update Methods.java

--- Methods.java
+++ Methods.java
@@ -1,65 +1,47 @@
+package methods;
 
 public class Main {
 
 	public static void main(String[] args) {
-
-		int result = total2(2, 3);
-		add();
-		delete();
-		update();
-		total();
-		giveCity();
-
-		System.out.println(total());
-		System.out.println(giveCity());
-		System.out.println(result);
-		System.out.println(total3(2, 7, 8, 9));
+		
+		findNumber();
 
 	}
-
-	public static void add() {
-
-		System.out.println("Added.");
-	}
-
-	public static void delete() {
-
-		System.out.println("Deleted.");
-	}
-
-	public static void update() {
-
-		System.out.println("Updated.");
-
-	}
-
-	public static int total() {
-
-		return 5;
-
-	}
-
-	public static String giveCity() {
-
-		return "İstanbul";
-
-	}
-
-	public static int total2(int number, int number2) {
-
-		return number + number2;
-	}
-
-	public static int total3(int... numbers) {
-
-		int number = 0;
-
+	
+	public static void findNumber() {
+		
+		int[] numbers = {1,2,3,4,5,6,7,8,9,0};
+		int searchNumber = 5;
+		boolean save = false;
+		
 		for (int i : numbers) {
-
-			number += i;
+			
+			if (numbers[i] == searchNumber) {
+				
+				save = true;
+				
+				break;
+				
+			}
+		
 		}
-
-		return number;
+		
+		if (save == true) {
+			
+			messege("Aranan sayı bulundu :"+ searchNumber);
+			
+		} else {
+			
+			messege(searchNumber +" sayısı bulunamadı.");
+			
+		}
+		
+		
+	}
+	
+	public static void messege(String messege) {
+		
+		System.out.println(messege);
 	}
 
 }
