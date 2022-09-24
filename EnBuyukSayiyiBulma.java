93af332178475eb61ca7e7100ec24b110fc91e3e
ferhat-atas <93721598+ferhat-atas@users.noreply.github.com>
2022-09-24 17:06:07 +0300

    Create EnBuyukSayiyiBulma.java

---
+++ EnBuyukSayiyiBulma.java
@@ -0,0 +1,33 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int sayi3 = 36;
+		int sayi1 = 38;
+		int sayi2 = 37;
+		
+		
+		if (sayi3>=sayi2) {
+			
+			if(sayi3>=sayi1) {
+				System.out.println("En büyük sayı= "+sayi3);
+			}
+				
+			}else if(sayi2>=sayi1) {
+				
+				if(sayi2>=sayi3) {
+					System.out.println("En büyük sayı= "+sayi2);
+				}
+			
+			}else if(sayi1>=sayi3) {
+				
+				if(sayi1>=sayi2) {
+					System.out.println("En büyük sayı= "+sayi1);
+				}
+			
+			}
+		
+	}
+
+}
